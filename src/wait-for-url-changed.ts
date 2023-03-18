19be1326c1fc884ff01b4ce87a0ba1d0ae860188
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2023-03-18 11:05:39 +0000

    chore: upgrade dependencies

--- src/wait-for-url-changed.ts
+++ src/wait-for-url-changed.ts
@@ -1,6 +1,6 @@
-import { observeUrlChanges } from '@blackglory/observe'
+import { observeURLChanges } from '@blackglory/observe'
 import { firstValueFrom } from 'rxjs'
 
 export async function waitForUrlChanged(): Promise<void> {
-  await firstValueFrom(observeUrlChanges())
+  await firstValueFrom(observeURLChanges())
 }
