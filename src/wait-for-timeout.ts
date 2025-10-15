3a6bb381699cd2e3677c0b3f655fedce41301f0b
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2025-10-15 16:11:01 +0000

    refactor(wait-for-timeout): use `delay` from `extra-promise`

--- src/wait-for-timeout.ts
+++ src/wait-for-timeout.ts
@@ -1,15 +1,5 @@
-import { setTimeout } from 'extra-timers'
+import { delay } from 'extra-promise'
 
 export function waitForTimeout(ms: number, signal?: AbortSignal): Promise<void> {
-  return new Promise((resolve, reject) => {
-    if (signal?.aborted) return reject(signal.reason)
-
-    const cancel = setTimeout(ms, resolve)
-
-    signal?.addEventListener('abort', async () => {
-      cancel()
-
-      reject(signal.reason)
-    })
-  })
+  return delay(ms, signal)
 }
