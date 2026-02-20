3bf298c20556c418f180b1ccffb49c777a0a69b3
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2026-02-20 19:00:31 +0000

    feat(wait-for-all-macrotasks-processed): await `setImmediate` and `setTimeout(0)` at the same time

--- src/wait-for-all-macrotasks-processed.ts
+++ src/wait-for-all-macrotasks-processed.ts
@@ -1,5 +1,9 @@
-import { setImmediate } from 'extra-timers'
+import { setImmediate, setTimeout } from 'extra-timers'
 
-export function waitForAllMacrotasksProcessed(): Promise<void> {
-  return new Promise<void>(resolve => setImmediate(resolve))
+export async function waitForAllMacrotasksProcessed(): Promise<void> {
+  // `setTimeout(0)`和`setImmediate`的回调调用顺序是不确定的, 因此等待两个回调都完成.
+  await Promise.all([
+    new Promise<void>(resolve => setImmediate(resolve))
+  , new Promise<void>(resolve => setTimeout(0, resolve))
+  ])
 }
